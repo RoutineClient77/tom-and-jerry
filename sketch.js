457c399d40d0786463891ba66e895a951c2392c8
RoutineClient77 <77229720+RoutineClient77@users.noreply.github.com>
2021-02-07 15:51:51 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,53 @@
 var cat, mouse;
-var backgroundImg, garden;
-var catImg1, catImg2, catImg3;
-var mouseImg1, mouseImg2, mouseImg3;
+var garden, gardenImg; 
+var cat1, cat2, cat3;
+var mouse1, mouse2, mouse3;
+
+
 function preload() {
-  backgroundImg = loadImage("images/garden.png");
-  catImg1= loadAnimation("images/cat1.png");
-  catImg2=loadAnimation("images/cat2.png","images/cat3.png");
-  catImg3= loadAnimation("images/cat4.png");
-  mouseImg1=loadAnimation("images/mouse1.png");
-  mouseImg2= loadAnimation("images/mouse2.png","images/mouse3.png");
-  mouseImg3=loadAnimation("images/mouse4.png"); 
-} 
-function setup(){
-  createCanvas(1000,800);
-  cat = createSprite(870, 600);
-  cat.addAnimation("tomSleeping", catImg1);
-  cat.scale = 0.2;
-  mouse = createSprite(200, 600);
-  mouse.addAnimation("jerryStanding", mouseImg1);
-  mouse.scale = 0.15; 
+    cat1.loadImage("cat1.png");
+    cat2.loadAnimation("cat2.png" , "cat3.png");
+    cat3.loadImage("cat4.png");
+
+    mouse1.loadImage("mouse1.png");
+    mouse2.loadAnimation("mouse2.png" , "mouse3.png");
+    mouse3.loadImage("mouse4.png");
+
+    gardenImg.loadImage("garden.png");
+
+
+
+
+
 }
-function draw(){
-  background(backgroundImg);
-  //Write condition here to evalute if tom and jerry collide
-  if(cat.x - mouse.x < (cat.width - mouse.width)/2) { 
-    cat.velocityX=0;
-    cat.addAnimation("tomLastImage", catImg3);
-    cat.x =300; cat.scale=0.2;
-    cat.changeAnimation("tomLastImage");
-    mouse.addAnimation("jerryLastImage", mouseImg3);
-    mouse.scale=0.15;
-    mouse.changeAnimation("jerryLastImage");
-  } 
-  drawSprites(); 
-} 
-function keyPressed(){ 
-  //For moving and changing animation write code here 
-  if(keyCode === LEFT_ARROW){ 
-   cat.velocityX = -5;
-   cat.addAnimation("tomRunning", catImg2);
-   cat.changeAnimation("tomRunning");
-   mouse.addAnimation("jerryTeasing", mouseImg2);
-   mouse.frameDelay = 25;
-   mouse.changeAnimation("jerryTeasing"); 
-  } 
+
+function setup(){
+    createCanvas(1000,800);
+    
+    cat = createSprite(150,150,10,10);
+    cat.addImage(cat1);
+
+    mouse = createSprite(850,850,10,10);
+    cat.addImage(mouse1);
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  if(keyCode === LEFT_ARROW){
+      cat.VELOCITYx = -5;
+      cat.addAnimation("catRunning", cat2);
+      cat.changeAnimation("catRunning");
+  }
+
+
 }
